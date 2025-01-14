b674b59e8f4970fba4f28e1bc73fcdab86956ea4
Mohammad Amadeus Andika Fadhil <150133437+Mazdeus@users.noreply.github.com>
2025-01-14 20:10:47 +0700

    Add files via upload

---
+++ Java_Dasar/src/string1.java
@@ -0,0 +1,10 @@
+public class string1 {
+    public static void main(String[] args) {
+        String greeting = "Hello World!";
+        System.out.println(greeting);
+
+        char[] dicodingChars = { 'd', 'i', 'c', 'o', 'd', 'i', 'n', 'g' };
+        String dicodingString = new String(dicodingChars);
+        System.out.println(dicodingString);
+    }
+}
